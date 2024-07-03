9375db35fee1823535785a53c6237ebd46562b7f
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-07-03 05:00:50 +0000

    java numbers

--- Numbers/palin.java
+++ Numbers/palin.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 class palin{
-    public static void main(String[]args){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the number:");
-        int n=s.nextInt();
-    
+    static boolean ispalindrome(int n){
         int org=n;
         int rev=0;
-        
-        
         while(n>0){
             int d=n%10;
             rev=rev*10+d;
             n=n/10;
         }
         if(org==rev){
-            System.out.println("The entered number is palindrome");
-
+            return true;
         }
         else{
-            System.out.println("The number is not palindrome");
+            return false;
         }
-    }
-
-
-
         
 
+
+    }
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number u want to chehck:");
+        int n=s.nextInt();
+        System.out.println(ispalindrome(n));
+
+
+    }
 }
