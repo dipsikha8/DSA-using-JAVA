e0f98d9ed6cec8c114621e01fa85e8a8589bb4d5
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-07-12 03:59:01 +0000

    array

---
+++ ARRAYS/Array_Left_Rotate_D_Place.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Array_Left_Rotate_D_Place{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+ 
+        int size=s.nextInt();
+        int[] a=new int[size];
+        for(int i=0;i<size;i++){
+            a[i]=s.nextInt();
+        }
+        System.out.println("Array is :");
+        for(int i=0;i<size;i++){
+            System.out.println(a[i]);
+        }
+        System.out.println("Array after Rotation:");
+    }
+    static void Rotate(int[] a){
+        int temp=
+    }
+}
